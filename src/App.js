bb1da9229b305b83bb9909844cd76ea94b515dd2
Chaedie <chaedong.im.dev@gmail.com>
2022-07-08 10:17:05 +0900

    learn : cleanUpFunction 학습

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import styles from './App.module.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  useEffect(() => console.log('I run only once.'), []);
-  useEffect(() => console.log('I run when "keyword" changes.'), [keyword]);
-  useEffect(() => console.log('I run when "counter" changes.'), [counter]);
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  function Hello() {
+    useEffect(() => {
+      console.log('hi :)');
+      return () => console.log('bye :(');
+    }, []);
+    return <h1>Hello</h1>;
+  }
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" plasceholder="Search here..." name="" id="" />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }

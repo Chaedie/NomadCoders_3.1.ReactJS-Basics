defb00ddcaa2621a9c3dba8cd156f20a8b4a338c
Chaedie <chaedong.im.dev@gmail.com>
2022-07-08 09:59:22 +0900

    learn : useEffect 학습

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Button from './Button';
 import styles from './App.module.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => console.log('I run only once.'), []);
+  useEffect(() => console.log('I run when "keyword" changes.'), [keyword]);
+  useEffect(() => console.log('I run when "counter" changes.'), [counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!</h1>
-      <Button text={'hello'}></Button>
+      <input value={keyword} onChange={onChange} type="text" plasceholder="Search here..." name="" id="" />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }

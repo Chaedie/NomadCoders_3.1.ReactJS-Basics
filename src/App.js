18654853b56f3a1d68c67a05a0bff87edd58bc4b
Chaedie <chaedong.im.dev@gmail.com>
2022-07-08 12:39:37 +0900

    feat : To Do LIst 구현을 통해 React.js 기초 복습

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import styles from './App.module.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  function Hello() {
-    useEffect(() => {
-      console.log('hi :)');
-      return () => console.log('bye :(');
-    }, []);
-    return <h1>Hello</h1>;
-  }
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos([toDo, ...toDos]);
+    setToDo('');
+  };
+  console.log(toDos);
   return (
     <div>
-      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
-      {showing ? <Hello /> : null}
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit} action="">
+        <input onChange={onChange} value={toDo} type="text" placeholer="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((x, i) => (
+          <li key={i}>{x}</li>
+        ))}
+      </ul>
     </div>
   );
 }

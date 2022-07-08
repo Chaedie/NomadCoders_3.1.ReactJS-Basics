3fab900b6c212d9a62b540e9db701be16a3056b6
Chaedie <chaedong.im.dev@gmail.com>
2022-07-08 17:06:51 +0900

    feat : react-router-dom 을 활용해 Routing 구현

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import Button from './Button';
-import styles from './App.module.css';
 import { useState, useEffect } from 'react';
 
-function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then((res) => res.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
   return (
-    <div>
-      <h1>The coins! ({coins.length})</h1>
-      {loading ? <strong> Loading...</strong> : null}
-      <ul>
-        {coins.map((coin, i) => (
-          <li key={coin.id}>
-            {coin.name} ({coin.symbol}) = {coin.quotes.USD.price.toFixed(2)}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
   );
 }
 
